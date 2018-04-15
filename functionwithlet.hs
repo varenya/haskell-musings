8470fafc4d929de7fd6d43add42d160c610dc5cc
Varenya <varen90@gmail.com>
2018-04-15 22:02:20 +0530

    adding a absoulute method

--- functionwithlet.hs
+++ functionwithlet.hs
@@ -6,5 +6,10 @@ add a b = a + b
 
 divide a b = a `div` b
 
+myAbs :: Integer -> Integer
+myAbs x
+    | x < 0  = (-x)
+    | otherwise = x
+
 printInc2 n = let plusTwo = n + 2
               in print plusTwo
